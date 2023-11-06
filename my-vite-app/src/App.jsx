19962c3e607f8e64020031d1c42a5189e107b8ca
+ <+>
2023-11-06 08:50:35 +0300

    done

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/signup" element={<Signup />} />
           <Route path="/Homepage" element={<Homepage/>} />
-          <Route path="/About" element={<About/>} />
+          {/* <Route path="/About" element={<About/>} /> */}
 
 
         
