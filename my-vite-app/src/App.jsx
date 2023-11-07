cc2fb4bcbfc6d19b883b8ef80fcf7ce4aefe52b8
Dkabucho <daniel.kabucho@moringschool.com>
2023-11-07 20:33:24 +0300

    Navbar

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 // import Login from './components/Login';
 import Signup from './components/Signup';
 import Welcome from './components/Welcome';
 import Homepage from './components/Homepage';
-import Navbar from './components/Navbar';
 import AboutUs from './components/AboutUs';
 import Charities from './components/Charities';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
- 
-     
       <Router>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Homepage/>} />
-          <Route path="/welcome" element={<Welcome />} /> 
-          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/" element={<Homepage />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/about" element={<AboutUs/>} /> 
-          <Route path="/charities" element={<Charities/>} />
-        
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/charities" element={<Charities />} />
         </Routes>
       </Router>
-      {/* footer */}
     </div>
   );
 }
+
+
 
 export default App;
 
