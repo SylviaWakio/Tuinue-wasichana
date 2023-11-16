eb0a58d8c4dc6ae73e0291b2093f0c2547b14a5b
Dkabucho <daniel.kabucho@moringschool.com>
2023-11-16 17:16:15 +0300

    complete

--- my-vite-app/src/components/Donate.jsx
+++ my-vite-app/src/components/Donate.jsx
@@ -2,73 +2,87 @@ import React, { useState } from "react";
 import "./Donate.css";
 
 const Donate = () => {
-  const [donorId, setDonorId] = useState("");
   const [isAnonymous, setIsAnonymous] = useState(false);
-  const [isOneTimeDonation, setIsOneTimeDonation] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [amount, setAmount] = useState(0);
-  const [charityId, setCharityId] = useState("");
   const [isDonationSubmitted, setDonationSubmitted] = useState(false);
 
-  const handleDonate = async (event) => {
+  const handleDonate = (event) => {
     event.preventDefault();
 
-    if ((donorId && phoneNumber && amount > 0 && charityId)) {
-      try {
-        const response = await fetch('https://tuinuewasichana.onrender.com/donations/donate', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            donor_id: donorId,
-            phone_number: phoneNumber,
-            amount,
-            charity_id: charityId,
-            is_anonymous: isAnonymous,  // Directly use the boolean value
-            is_one_time_donation: isOneTimeDonation,  // Directly use the boolean value
-          }),
-        });
+    if (isAnonymous || (name && email && phoneNumber && amount > 0)) {
+      console.log(name)
+      console.log(email)
+      console.log(phoneNumber)
+      console.log(amount)
+      fetch('https://tuinuewasichana.onrender.com/donations/donate', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
 
-        if (response.ok) {
-          // Reset the form and provide feedback to the user upon successful donation
-          setDonorId("");
-          setIsAnonymous(false);
-          setIsOneTimeDonation(false);
-          setPhoneNumber("");
-          setAmount(0);
-          setCharityId("");
-          setDonationSubmitted(true);
-        } else {
-          const responseData = await response.json();
-          throw new Error(`Failed to submit donation: ${responseData.error}`);
-        }
-      } catch (error) {
-        alert(`Failed to submit donation: ${error.message}`);
-      }
-    } else {
-      alert("Please fill in all the required fields.");
+          donor_id: 1,
+          amount,
+          phone_number: phoneNumber,
+          is_anonymous: isAnonymous,
+          charity_id: 1,
+          is_one_time_donation:isDonationSubmitted
+        })
+      }).then(response => {
+        response.json()
+      }).then(data => {
+        console.log(data)
+      }).catch(erro => console.log(erro))
+      alert("Donation Successfully Done")
     }
+
+
+    // if (isAnonymous || (name && email && phoneNumber && amount > 0)) {
+    //   fetch('https://tuinuewasichana.onrender.com/donations/donate', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       name: isAnonymous ? 'Anonymous' : name,
+    //       email,
+    //       phoneNumber,
+    //       amount,
+    //     }),
+    //   })
+    //     .then((response) => {
+    //       if (response.ok) {
+    //         // Reset the form and provide feedback to the user upon successful donation
+    //         setIsAnonymous(false);
+    //         setName("");
+    //         setEmail("");
+    //         setPhoneNumber("");
+    //         setAmount(0);
+    //         setDonationSubmitted(true);
+    //       } else {
+    //         throw new Error('Failed to submit donation');
+    //       }
+    //     })
+    //     .catch((error) => {
+    //       alert(`Failed to submit donation: ${error.message}`);
+    //     });
+    // } else {
+    //   alert("Please fill in all the required fields.");
+    // }
   };
 
   return (
     <div className="page-container">
       <div className="form-container">
-        <h2>Donate Page</h2>
-        {isDonationSubmitted && <p>Thank you for your donation!</p>}
-        <form onSubmit={handleDonate}>
-          <div>
-            <label>
-              Donor ID:
-              <input
-                type="text"
-                value={donorId}
-                onChange={(e) => setDonorId(e.target.value)}
-                required
-              />
-            </label>
-          </div>
 
+        <h2>Donate Page</h2>
+        {isDonationSubmitted && (
+          <p>Thank you for your donation!</p>
+        )}
+        <form onSubmit={handleDonate}>
           <div>
             <label>
               <input
@@ -80,52 +94,53 @@ const Donate = () => {
             </label>
           </div>
 
-          <div>
-            <label>
-              Charity ID:
-              <input
-                type="text"
-                value={charityId}
-                onChange={(e) => setCharityId(e.target.value)}
-                required
-              />
-            </label>
-          </div>
+          {!isAnonymous && (
+            <div>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </label>
+              <br />
 
-          <div>
-            <label className="pn">
-              Phone Number:
-              <input
-                type="tel"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                required
-              />
-            </label>
-          </div>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </label>
+              <br />
 
-          <div>
-            <label className="ad">
-              Amount to Donate:
-              <input
-                type="number"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                required
-              />
-            </label>
-          </div>
+              <label className="pn">
+                Phone Number:
+                <input
+                  type="tel"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  required
+                />
+              </label>
+              <br />
 
-          <div>
-            <label>
-              <input
-                type="checkbox"
-                checked={isOneTimeDonation}
-                onChange={() => setIsOneTimeDonation(!isOneTimeDonation)}
-              />
-              One-Time Donation
-            </label>
-          </div>
+              <label className="ad">
+                Amount to Donate:
+                <input
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  required
+                />
+              </label>
+              <br />
+            </div>
+          )}
 
           <button type="submit">Donate</button>
         </form>
